2c2878a15653ac7d5569325c51ce7bec2ff8e3dd
Nestor Mosquera <yunsde18@gmail.com>
2022-01-29 17:02:32 +0100

    new update

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -3,13 +3,13 @@ import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-dotenv.config({
-  path: path.resolve(__dirname, process.env.NODE_ENV + '.env'),
-});
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+// dotenv.config({
+//   path: path.resolve(__dirname, process.env.NODE_ENV + '.env'),
+// });
 
 export default {
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    // NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 4200,
     mongodbURL:
     process.env.MONGODB_URI ||
