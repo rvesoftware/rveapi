89ed742937abd3d017d1a082ba9be75d5456f32b
Nestor Mosquera <yunsde18@gmail.com>
2022-01-29 16:37:05 +0100

    new update

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,3 +1,14 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+dotenv.config({
+  path: path.resolve(__dirname, process.env.NODE_ENV + '.env'),
+});
+
 export default {
     NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || 4200,
