f5e9af0621d74bd45f11f6f533d105c8306e6c5d
Nestor Mosquera <yunsde18@gmail.com>
2022-01-29 17:59:32 +0100

    new upd

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -3,18 +3,18 @@ import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-// const __dirname = dirname(fileURLToPath(import.meta.url));
-// dotenv.config({
-//   path: path.resolve(__dirname, process.env.NODE_ENV + '.env'),
-// });
+const __dirname = dirname(fileURLToPath(import.meta.url));
+dotenv.config({
+  path: path.resolve(__dirname, process.env.NODE_ENV + '.env'),
+});
 
 export default {
     // NODE_ENV: process.env.NODE_ENV || 'development',
-    // PORT: process.env.PORT || 4200,
+    PORT: process.env.PORT || 4200,
     mongodbURL:
     process.env.MONGODB_URI ||
     'mongodb+srv://user:api12345@cluster0.2tqpl.mongodb.net/apirest?retryWrites=true&w=majority',
-    // SKIP_PREFLIGHT_CHECK:true,
+    SKIP_PREFLIGHT_CHECK:true,
     // MAILGUN_APIKEY:'a5e2f6a88af9e5de3261717aa90f7df0-e31dc3cc-65a8594d',
     // JWT_ACC_ACTIVATE:'accountactivatekey123',
     // CLIENT_URL:'http://localhost:3000',
