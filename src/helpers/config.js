049090e5ecea9dacb33f150ffc323b35f9d571bc
Nestor Mosquera <yunsde18@gmail.com>
2022-01-29 16:57:30 +0100

    new update

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -4,7 +4,6 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
 dotenv.config({
   path: path.resolve(__dirname, process.env.NODE_ENV + '.env'),
 });
