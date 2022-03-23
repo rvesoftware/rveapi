9807e8c91e5140796306326eeb9f8932bf56aba6
Nestor Mosquera <yunsde18@gmail.com>
2022-03-23 11:32:24 -0500

    update chat

--- src/api/v0/helpers/config.js
+++ src/api/v0/helpers/config.js
@@ -20,9 +20,9 @@ export default {
     // CLIENT_URL:'http://localhost:3000',
     // RESET_PASSWORD_KEY:'passwordresetkey123456',
     JWT_SIGNIN_KEY:'signinkey1234',
-    STREAM_API_KEY: 'pw6btk4aq5wt',
-    STREAM_API_SECRET: '5vknznhxb3jenzys5gkjmc7shvew7e6fzyp4yktng6peaa8cjqp558zaxcmmuytk',
-    STREAM_APP_ID: 1169200,
+    STREAM_API_KEY: 'w9gxsq46p828',
+    STREAM_API_SECRET: '8bvx85kh5xkdet9ca5rvtzw3cjzc7vvypz9xjn8s522e2qjsq6ycsczpmj8k45nw',
+    STREAM_APP_ID: 1176292,
     accessKeyId: process.env.accessKeyId || 'AKIAVHJK4GQ6BBLS6M62',
     secretAccessKey:
     process.env.secretAccessKey || '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',
